5b330ade2bedf956fc1eb89e1bcdec369de7fc71
Sakoda, Taro <sakoda.taro.229@gmail.com>
2024-12-31 15:12:42 +0900

    ✨ Update root command description for expense tracker

--- cmd/root.go
+++ cmd/root.go
@@ -11,13 +11,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "expense-tracker",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A simple expense tracker to manage your finances.",
+	Long: "",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -43,5 +38,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
