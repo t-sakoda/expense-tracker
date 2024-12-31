2bf7bf18cbc975d9ecf17e134d22dfa90178ebd0
Sakoda, Taro <sakoda.taro.229@gmail.com>
2024-12-31 14:22:26 +0900

    ✨ Implement CLI commands: add, delete, list, summary, and root for expense tracker

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-   "fmt"
-)
+import "github.com/t-sakoda/expense-tracker/cmd"
 
 func main() {
-   fmt.Println("Hello World!")
+	cmd.Execute()
 }
